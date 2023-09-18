66f6cb7b2f7f405164397ffb8f379d79b89a17fd
WojciechFlak <wojciech-flak@wp.pl>
2023-09-18 20:17:55 +0200

    Mock, MonkeyPach and skip

--- code/pytest_tutorial/test_twitter.py
+++ code/pytest_tutorial/test_twitter.py
@@ -1,6 +1,13 @@
 import pytest
+import requests
 
 from twitter import Twitter
+
+
+class ResponseGetMock(object):
+    @staticmethod
+    def json():
+        return {'avatar_url': 'test'}
 
 
 # Fixture
@@ -16,19 +23,33 @@ def prepare_backend_file(tmpdir):
     return temp_file
 
 
+@pytest.fixture(autouse=True)
+def no_requests(monkeypatch):
+    monkeypatch.delattr('requests.sessions.Session.request')
+
+
 # @pytest.fixture(autouse=True)
 # def prepare_backend_file():
 #     # auto-use runs before every test and is considered as unsafe method
 #     with open('test.txt', 'w') as backend_file:
 #         pass
 
+@pytest.fixture(params=[None, 'python'])
+def username(request):
+    return request.param
+
 
 @pytest.fixture(params=['list', 'prepare_backend_file'], name='twitter')
-def fixture_twitter(prepare_backend_file, request):
+def fixture_twitter(prepare_backend_file, username, request, monkeypatch):
     if request.param == 'list':
-        twitter = Twitter()
+        twitter = Twitter(username=username)
     elif request.param == 'prepare_backend_file':
-        twitter = Twitter(backend=prepare_backend_file)
+        twitter = Twitter(backend=prepare_backend_file, username=username)
+
+    def monkey_return(url):
+        return ResponseGetMock()
+
+    monkeypatch.setattr(requests, 'get', monkey_return)
     return twitter
 
 
@@ -43,7 +64,7 @@ def test_single_message(twitter):
     # When
     twitter.tweet('TEST MESSAGE 1')
     # Then
-    assert twitter.tweets == ['TEST MESSAGE 1']
+    assert twitter.tweet_messages == ['TEST MESSAGE 1']
 
 
 def test_long_message(twitter):
@@ -52,11 +73,12 @@ def test_long_message(twitter):
     # twitter = Twitter()
     with pytest.raises(Exception):
         twitter.tweet('test'*41)
-    assert twitter.tweets == []
+    assert twitter.tweet_messages == []
 
 
 def test_tweet_with_hashtag(twitter):
     assert 'the' in twitter.find_hashtag('This is #the first message wish hashtag')
+
 
 def test_initialize_two_twitter_classes(prepare_backend_file):
     twitter1 = Twitter(backend=prepare_backend_file)
@@ -65,7 +87,7 @@ def test_initialize_two_twitter_classes(prepare_backend_file):
     twitter1.tweet('Test message1')
     twitter2.tweet('Test message2')
 
-    assert twitter1.tweets == ['Test message1', 'Test message2']
+    assert twitter2.tweet_messages == ['Test message1', 'Test message2']
 
 
 # Parametrize
@@ -79,3 +101,10 @@ def test_initialize_two_twitter_classes(prepare_backend_file):
 def test_different_hashtags(twitter, message, expected):
     # twitter = Twitter()
     assert twitter.find_hashtag(message) == expected
+
+
+def test_tweet_with_username(twitter):
+    if not twitter.username:
+        pytest.skip()
+    twitter.tweet('Test message')
+    assert twitter.tweets == [{'message': 'Test message', 'avatar': 'test'}]
